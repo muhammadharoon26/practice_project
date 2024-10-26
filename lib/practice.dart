8ba9f08c749102dd744204635c773ace608ecfd5
Muhammad Haroon <mharoon1326@gmail.com>
2024-10-26 16:20:28 +0500

    init

--- lib/practice.dart
+++ lib/practice.dart
@@ -4,17 +4,45 @@ import 'dart:io';
 
 void main() {
   print('Hello, World!');
-  var myCalculator = Calculator();
-  myCalculator.printName('Haroon');
-  print(myCalculator.add(90, 100));
+  var myC = C();
+  myC.printName('Haroon');
+  print(myC.add(90, 100));
+  myC.printList();
 }
 
-class Calculator {
-  void printName(String name) {
-    print('Name: $name');
-  }
-
+class C {
   int add(int a, int b) {
     return a + b;
+  }
+
+  void printList() {
+    var listNums = [1, 2, 3, 4];
+    listNums.add(5);
+    var names = [];
+    names.addAll(listNums);
+    names.add('Haroon');
+    names.insert(2, 100);
+    print(listNums);
+    print(names);
+    names.insertAll(3, listNums);
+    print(names);
+    names[2] = 'Muhammad Haroon';
+    print(names);
+    names.remove('Muhammad Haroon');
+    print(names);
+    names.removeAt(10);
+    print(names);
+    names.removeLast();
+    print(names);
+    names.removeRange(2, 7);
+    print(names);
+    names.removeAt(0);
+    print(names);
+    names.removeRange(1, 3);
+    print(names);
+  }
+
+  void printName(String name) {
+    print('Name: $name');
   }
 }
