9d12cdaaa8f7a10be42e847b2012cf006b6c0a00
Muhammad Haroon <mharoon1326@gmail.com>
2024-10-30 23:44:49 +0500

    "Removed duplicate Column widgets and added conditional background color to ElevatedButtons."

--- lib/columnrowWidget.dart
+++ lib/columnrowWidget.dart
@@ -17,24 +17,6 @@ class _ColumnRowAppState extends State<ColumnRowApp> {
     return MaterialApp(
         home: Scaffold(
             body: Column(children: [
-      Column(children: [
-        Text('Hi, its a Column !!!'),
-        Row(children: [Text('Hi, Its a Row !!!')]),
-        Row(children: [Text('Hi, Its a Row !!!')]),
-        Row(children: [Text('Hi, Its a Row !!!')])
-      ]),
-      Column(children: [
-        Text('Hi, its a Column !!!'),
-        Row(children: [Text('Hi, Its a Row !!!')]),
-        Row(children: [Text('Hi, Its a Row !!!')]),
-        Row(children: [Text('Hi, Its a Row !!!')])
-      ]),
-      Column(children: [
-        Text('Hi, its a Column !!!'),
-        Row(children: [Text('Hi, Its a Row !!!')]),
-        Row(children: [Text('Hi, Its a Row !!!')]),
-        Row(children: [Text('Hi, Its a Row !!!')])
-      ]),
       Column(
         children: [
           ElevatedButton(
@@ -54,10 +36,7 @@ class _ColumnRowAppState extends State<ColumnRowApp> {
                   },
                 ),
               ),
-              child: Text(
-                'Row',
-                selectionColor: Colors.amber,
-              ),
+              child: Text('Row'),
               onPressed: () {
                 print('Row Button Pressed');
               },
@@ -66,21 +45,39 @@ class _ColumnRowAppState extends State<ColumnRowApp> {
           ]),
           Row(children: [
             ElevatedButton(
+              style: ButtonStyle(
+                backgroundColor: WidgetStateProperty.resolveWith<Color>(
+                  (states) {
+                    if (states.contains(WidgetState.pressed))
+                      return Colors.green;
+                    return Colors.amberAccent;
+                  },
+                ),
+              ),
               child: Text('Row'),
               onPressed: () {
                 print('Row Button Pressed');
               },
             ),
-            SizedBox(height: 50),
+            SizedBox(height: 50)
           ]),
           Row(children: [
             ElevatedButton(
+              style: ButtonStyle(
+                backgroundColor: WidgetStateProperty.resolveWith<Color>(
+                  (states) {
+                    if (states.contains(WidgetState.pressed))
+                      return Colors.green;
+                    return Colors.amberAccent;
+                  },
+                ),
+              ),
               child: Text('Row'),
               onPressed: () {
                 print('Row Button Pressed');
               },
             ),
-            SizedBox(height: 50),
+            SizedBox(height: 50)
           ])
         ],
       )
